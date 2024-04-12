1128c8728eeddab21cf01dee36c9383c14b212ed
djm-exe <josh.socials4@gmail.com>
2024-04-12 17:10:32 +0800

    added navigation to other screens

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-import flet as fl 
+import flet as ft
 from router import views_handler
 
 
 def main(page: ft.Page):
     def route_change(route):
         page.views.clear()
-        page.views.append(views_handler(page)[page.route])
+        page.views.append(
+            ft.View(
+                route='/',
+                controls=[
+                    ft.Text(value='Password Vault', size=50),
+                    ft.ElevatedButton(text='Register', on_click=lambda _: page.go('/register')),
+                    ft.ElevatedButton(text='Login', on_click=lambda _: page.go('/login'))
+                ]
+            )
+        )
+        if page.route =='/register':
+            page.views.append(
+                views_handler(page)[page.route]
+            )
+
+        page.update()
+
+    def view_pop(e: ft.ViewPopEvent):
+        page.views.pop()
+        top_view: ft.View = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+    
 
 
 ft.app(target=main)
