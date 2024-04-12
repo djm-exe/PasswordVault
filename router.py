8bc89890ffcdc0ba578909d911885a78f9b982fc
djm-exe <josh.socials4@gmail.com>
2024-04-12 21:43:17 +0800

    fixed a misspelling

--- router.py
+++ router.py
@@ -2,7 +2,7 @@ import flet as ft
 from screens.login import LoginScreen
 from screens.register import RegisterScreen
 
-def view_handler(page):
+def views_handler(page):
     return{
         '/login': ft.View(route='/login', controls=[LoginScreen(page)]),
         '/register': ft.View(route='/register', controls=[RegisterScreen(page)])
