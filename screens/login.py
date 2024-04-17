e7519786177166852f40607aed6e8744d66dbcc1
djm-exe <josh.socials4@gmail.com>
2024-04-18 00:42:12 +0800

    linked register screen to the database

--- screens/login.py
+++ screens/login.py
@@ -1,11 +1,18 @@
 import flet as ft
+from database import PasswordVault
 
 class LoginScreen(ft.Container):
     def __init__(self, page:ft.Page):
         super().__init__()
+
+
+        username = ft.TextField(label='Username', width= 150)
+        password = ft.TextField(label='Password', width= 150)
         self.content=ft.Column(
             controls=[
                 ft.Text("Login Screen"),
+                username,
+                password,
                 ft.ElevatedButton(text='Go Back', on_click= lambda _: page.go('/'))
             ]
         )
--- screens/register.py
+++ screens/register.py
@@ -1,13 +1,24 @@
 import flet as ft
+from database import PasswordVault
 
 class RegisterScreen(ft.Container):
     def __init__(self, page:ft.Page):
         super().__init__()
+
+        def create_vault(vault):
+            vault = PasswordVault(password.value)
+            vault.reg_user(username.value, password.value)
+            page.go('/login')
+            
+
+        username = ft.TextField(label="Username", width= 150)
+        password = ft.TextField(label="Password", width= 150)
         self.content=ft.Column(
             controls=[
                 ft.Text("Register Screen"),
-                ft.TextField(label="Username", width=150),
-                ft.TextField(label="Password", width=150),
+                username,
+                password,
+                ft.ElevatedButton(text="Create Vault", on_click= create_vault),
                 ft.ElevatedButton(text='Go Back', on_click= lambda _: page.go('/'))
-            ]
+            ],
         )
