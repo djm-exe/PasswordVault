84c99666fefb10f1d7cdf1c103eb882aeae5e271
djm-exe <josh.socials4@gmail.com>
2024-04-18 02:13:51 +0800

    updated

--- screens/vault.py
+++ screens/vault.py
@@ -5,12 +5,13 @@ class VaultScreen(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
 
-        # def LogOut(vault):
-        #     vault = PasswordVault()
+        def LogOut():
+            PasswordVault(master_password=None).__del__()
+            page.go('/')
 
         self.content=ft.Column(
             controls=[
                 ft.Text('Vault'),
-                ft.ElevatedButton(text='Logout')
+                ft.ElevatedButton(text='Logout', on_click= LogOut)
             ]
         )
