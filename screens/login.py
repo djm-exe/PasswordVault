de38d854e7e3648e03c5ed74482487b2c9738836
djm-exe <josh.socials4@gmail.com>
2024-06-18 11:07:42 +0800

    still debugging

--- screens/login.py
+++ screens/login.py
@@ -1,4 +1,3 @@
-from sqlite3 import IntegrityError
 import flet as ft
 from database import PasswordVault
 
@@ -6,28 +5,24 @@ class LoginScreen(ft.Container):
     def __init__(self, page:ft.Page):
         super().__init__()
 
-        def gotoVault(vault):
-
-            vault = PasswordVault(password.value)
-            try:
-                vault.login_user(username.value, password.value)
-                page.go('/vault')
-            except:
-                page.dialog = ft.AlertDialog(
-                    modal=True,
-                    content=ft.Text('Invalid username or password.'),
-                    actions=[ft.ElevatedButton(text='Okay')]
-                )
-
-
-        username = ft.TextField(label='Username', width= 150)
-        password = ft.TextField(label='Password', width= 150)
+        self.username = ft.TextField(label='Username', width= 150)
+        self.password = ft.TextField(label='Password', width= 150)
         self.content=ft.Column(
             controls=[
                 ft.Text("Login Screen"),
-                username,
-                password,
-                ft.ElevatedButton(text='Login', on_click= gotoVault),
+                self.username,
+                self.password,
+                ft.ElevatedButton(text='Login', on_click= lambda _: self.login(page, self.username.value, self.password.value)),
                 ft.ElevatedButton(text='Go Back', on_click= lambda _: page.go('/'))
             ]
         )
+
+    def login(self, page: ft.Page, username, password):
+        vault = PasswordVault(password)
+        if vault.login_user(username, password):
+            self.vault = vault
+            page.go('vault')
+        else:
+            password.error_text = "Invalid username or password. Try again."
+            page.update()
+
