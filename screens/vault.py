83b8d3e77a836ca1ab3643691986af283ecf5c5e
djm-exe <josh.socials4@gmail.com>
2024-06-22 07:23:00 +0800

    Added a floating button that adds item on-click

--- screens/vault.py
+++ screens/vault.py
@@ -5,13 +5,30 @@ class VaultScreen(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
 
-        def LogOut():
-            PasswordVault(master_password=None).__del__()
+        def LogOut(e):
+            PasswordVault('master_password').__del__()
             page.go('/')
 
-        self.content=ft.Column(
+        self.items_column = ft.Column()
+
+        self.content = ft.Column(
             controls=[
                 ft.Text('Vault'),
-                ft.ElevatedButton(text='Logout', on_click= LogOut)
-            ]
+                ft.Row(
+                    controls=[
+                        ft.ElevatedButton(text='Logout', on_click=LogOut)
+                    ],
+                    alignment=ft.MainAxisAlignment.END,
+                ),
+                ft.Row(
+                    controls=[
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_item)
+                    ]
+                ),
+                self.items_column  # Add this line
+            ],
         )
+
+    def add_item(self, e):
+        self.items_column.controls.append(ft.Text('New item'))
+        self.update()
