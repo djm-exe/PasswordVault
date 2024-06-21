24a3070602ace22b54bb8fa77dd6111933f50118
djm-exe <josh.socials4@gmail.com>
2024-06-22 02:00:45 +0800

    Improved the code

--- screens/login.py
+++ screens/login.py
@@ -7,6 +7,8 @@ class LoginScreen(ft.Container):
 
         self.vault = PasswordVault('master_password')
 
+        self.error_text = ft.Text(visible=False, color='red')
+
         self.username = ft.TextField(label='Username', width= 150)
         self.password = ft.TextField(label='Password', width= 150)
         self.content=ft.Column(
@@ -14,6 +16,7 @@ class LoginScreen(ft.Container):
                 ft.Text("Login Screen"),
                 self.username,
                 self.password,
+                self.error_text,
                 ft.ElevatedButton(text='Login', on_click= lambda _: self.login(page)),
                 ft.ElevatedButton(text='Go Back', on_click= lambda _: page.go('/'))
             ]
@@ -23,6 +26,7 @@ class LoginScreen(ft.Container):
         if self.vault.login_user(self.username.value, self.password.value):
             page.go('/vault')
         else:
-            self.password.error_text = "Invalid username or password. Try again."
+            self.error_text.value = "Invalid username or password. Try again."
+            self.error_text.visible = True
             page.update()
 
